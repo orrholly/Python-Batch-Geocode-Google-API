6b1b18c3111e8e98192950ff2e70a6f8848c2fe4
ho449 <holly.orr@nyu.edu>
2016-05-20 17:45:40 -0400

    read from csv addresses

--- testing_api.py
+++ testing_api.py
@@ -5,6 +5,15 @@ import pandas as pd
 import numpy as np
 
 from datetime import datetime
+fields = ['address']
+
+df = pd.read_csv('api_test.csv', skipinitialspace=True, usecols=fields)
+# See the keys
+# print df.keys()
+# See content in 'star_name'
+# print df.address
+addressValue = df.get_value(0,'address')
+print addressValue
 
 
 #Client key for google maps
@@ -12,10 +21,10 @@ gmaps = googlemaps.Client(key='AIzaSyDNpXx0jnXEWv1OHmdmTkOKPU72Ge1DOxk')
 
 #TESTING
 # testing hardcoded address string
-addressString = '1600 Amphitheatre Parkway, Mountain View, CA'
+# addressString = '1600 Amphitheatre Parkway, Mountain View, CA'
 
-# Testing Geocoding an address - testing one address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+# Geocoding an address - testing address read from csv
+geocode_result = gmaps.geocode(addressValue)
 
 #read csv address
 
@@ -26,16 +35,23 @@ j = json.dumps(geocode_result)
 print j
 
 getLocation = json.loads(j)
-# print getLocation [0]['geometry']['location']['lat']
+
+#grab latitude and longitude from json
+longVal = getLocation [0]['geometry']['location']['lng']
+#print longVal
 latVal = getLocation [0]['geometry']['location']['lat']
+# print latVal
 
 
+#create dictionary from address, long, and lat
 dict = {}
-dict.setdefault('address', []).append(addressString)
+dict.setdefault('address', []).append(addressValue)
+dict.setdefault('longitude', []).append(longVal)
 dict.setdefault('latitude', []).append(latVal)
+
 print dict
 raw_data = dict
-df = pd.DataFrame(raw_data, columns = ['address', 'latitude'])
+df = pd.DataFrame(raw_data, columns = ['address', 'longitude', 'latitude'])
 df.to_csv('example3.csv')
 
 
