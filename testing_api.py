8a3f187c3d0ddf4dd51dd0fb46cac417263e8b48
ho449 <holly.orr@nyu.edu>
2016-05-20 22:16:16 -0400

    test for no address found

--- testing_api.py
+++ testing_api.py
@@ -14,7 +14,7 @@ dict = {}
 
 #read csv address
 fields = ['address']
-df = pd.read_csv('warning_two.csv', skipinitialspace=True, usecols=fields)
+df = pd.read_csv('allwarnings_dc_nc_va.csv', skipinitialspace=True, usecols=fields)
 
 #this might be faster - keep until later
 # for r in df.itertuples():
@@ -30,36 +30,41 @@ df = pd.read_csv('warning_two.csv', skipinitialspace=True, usecols=fields)
 
 #iterate through rows
 for index, row in df.iterrows():
-    print row['address']
+    # print row['address']
     addressValue = row['address']
 
     # Geocoding an address - testing one address
     geocode_result = gmaps.geocode(addressValue)
 
-    #convert to json
-    j = json.dumps(geocode_result)
-    # print j['results']['geometry']['location']['lat']
-    # print j
+    #test to see if array is empty which means address does not exist and can't be geocoded
+    if not geocode_result:
+        # print "List is empty"
+        longVal = 0
+        latVal = 0
+    else:
+        #convert to json
+        j = json.dumps(geocode_result)
+        # print j['results']['geometry']['location']['lat']
+        # print j
 
-    getLocation = json.loads(j)
-
-    #grab latitude and longitude from json
-    longVal = getLocation [0]['geometry']['location']['lng']
-    #print longVal
-    latVal = getLocation [0]['geometry']['location']['lat']
-    # print latVal
+        getLocation = json.loads(j)
+        # print getLocation
+        #grab latitude and longitude from json
+        longVal = getLocation [0]['geometry']['location']['lng']
+        #print longVal
+        latVal = getLocation [0]['geometry']['location']['lat']
+        # print latVal
 
     #create dictionary from address, long, and lat
     dict.setdefault('address', []).append(addressValue)
     dict.setdefault('longitude', []).append(longVal)
     dict.setdefault('latitude', []).append(latVal)
-
+    print addressValue, longVal, latVal
 
 #output all values to csv
-# print dict
 raw_data = dict
 df = pd.DataFrame(raw_data, columns = ['address', 'longitude', 'latitude'])
-df.to_csv('example.csv')
+df.to_csv('geocode_south.csv')
 
 
 
